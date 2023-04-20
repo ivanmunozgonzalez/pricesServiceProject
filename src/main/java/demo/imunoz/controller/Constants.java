37e74780ed7af7dad9f16aec5480e48a4429afc0
ivanmunozgonzalez <124597560+ivanmunozgonzalez@users.noreply.github.com>
2023-04-20 08:19:51 +0200

    Delete src directory

--- src/main/java/demo/imunoz/controller/Constants.java
+++
@@ -1,9 +0,0 @@
-package demo.imunoz.controller;
-
-public final class Constants {
-
-    public static final String PRODUCT_ID_PARAMETER = "productId";
-    public static final String BRAND_ID_PARAMETER = "brandId";
-    public static final String DATE_APPLICATION_PARAMETER = "dateApplication";
-    public static final String DATE_FORMAT_PRODUCT_PRICE = "yyyy-MM-dd'T'HH:mm:ss";
-}
