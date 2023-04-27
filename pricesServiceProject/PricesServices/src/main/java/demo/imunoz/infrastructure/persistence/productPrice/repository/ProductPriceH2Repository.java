b2b636668bb3c10f47cc0a3dbcbbcb0d91f1d383
ivanmunozgonzalez <124597560+ivanmunozgonzalez@users.noreply.github.com>
2023-04-27 10:07:31 +0200

    Update ProductPriceH2Repository.java

--- pricesServiceProject/PricesServices/src/main/java/demo/imunoz/infrastructure/persistence/productPrice/repository/ProductPriceH2Repository.java
+++ pricesServiceProject/PricesServices/src/main/java/demo/imunoz/infrastructure/persistence/productPrice/repository/ProductPriceH2Repository.java
@@ -2,6 +2,7 @@ package demo.imunoz.infrastructure.persistence.productPrice.repository;
 
 import java.time.LocalDateTime;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import demo.imunoz.application.domain.exception.DomainException;
@@ -10,14 +11,15 @@ import demo.imunoz.application.domain.model.productPrice.ProductPrice;
 import demo.imunoz.application.ports.out.ProductPriceRepository;
 import demo.imunoz.infrastructure.persistence.productPrice.ProductPriceEntity;
 import demo.imunoz.infrastructure.persistence.productPrice.ProductPriceEntityMapper;
-import lombok.AllArgsConstructor;
 
 @Repository
-@AllArgsConstructor
 public class ProductPriceH2Repository implements ProductPriceRepository {
 
-	private final ProductPriceJPARepository productPriceJPARepository;
-	private final ProductPriceEntityMapper productPriceEntityMapper;
+	@Autowired
+	private ProductPriceJPARepository productPriceJPARepository;
+
+	@Autowired
+	private ProductPriceEntityMapper productPriceEntityMapper;
 
 	@Override
 	public ProductPrice findFirstProductPriceByDateOrderByPriority(LocalDateTime applicationDate, Long productId,
